d74f08b7ccf6994ac912a78b0e550cc91a19431d
briancha <briancha98@gmail.com>
2022-03-13 12:02:31 -0400

    adding linestrings to Leaflet

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -22,20 +22,20 @@
 // }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/light-v10',
+    id: 'mapbox/navigation-day-v1',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 });
 
 // We create the tile layer that will be the background of our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/navigation-night-v1',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -43,8 +43,8 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Light: light,
-    Dark: dark
+    Day: day,
+    Night: night
 };
 
 let map = L.map("mapid", {
@@ -52,7 +52,7 @@ let map = L.map("mapid", {
         44.0, -80.0
     ],
     zoom: 2,
-    layers: [light]
+    layers: [day]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -65,13 +65,33 @@ L.control.layers(baseMaps).addTo(map);
 let torontoData = "https://raw.githubusercontent.com/chabrian/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data, {
+//         onEachFeature: function(feature, layer) {
+//           console.log(layer);
+//           layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3><hr><h3>Airport name: " + feature.properties.name + "</h3>");
+//         }
+//   }).addTo(map);
+// });
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
+// Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
+        style: myStyle,
         onEachFeature: function(feature, layer) {
-          console.log(layer);
-          layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3><hr><h3>Airport name: " + feature.properties.name + "</h3>");
+        layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3><hr><h3>Destination: " + feature.properties.dst + "</h3>");
         }
-  }).addTo(map);
+    }).addTo(map);
 });
+// for styling, can also include the properties before onEachFeature()
+// or use layer.setStyle 
