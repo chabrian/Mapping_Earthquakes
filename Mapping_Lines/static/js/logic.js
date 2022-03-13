800ac2da05b41482a96fe9e42957a8014951fd76
briancha <briancha98@gmail.com>
2022-03-12 21:02:41 -0500

    adding lines to Leaflet

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -14,34 +14,65 @@ let map = L.map("mapid", {
 // Get data from cities.js
 let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
+// Coordinates for each point to be used in the line. (LAX to SFO)
+let route1 = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+];
+
+let route2 = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+let route3 = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+];
+
+//LAX to SFO
+L.polyline(route1, {
+    color: "red"
+}).addTo(map);
+
+// LAX to SFO to SLC to SEA
+L.polyline(route2, {
+    color: "yellow",
+    opacity: 0.5
+}).addTo(map);
+
+// SFO to AUS to YYZ to JFK
+L.polyline(route3, {
+    color: "blue",
+    dashArray: "5 10",
+    weight: 4,
+    opacity: 0.7
+}).addTo(map);
+
+
+// Loop through and create circleMarkers proportional to population
 // cityData.forEach(function(city) {
 //     console.log(city);
-//     L.marker(city.location)
+//     L.circleMarker(city.location, {
+//         color: 'orange',
+//         weight: 4,
+//         fillOpacity: 0.2,
+//         radius: city.population/200000
+//     })
 //     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 //     .addTo(map);
 //     // toLocaleString() will format with a thousands separator
 //    });
-
-// Loop through and create circleMarkers proportional to population
-cityData.forEach(function(city) {
-    console.log(city);
-    L.circleMarker(city.location, {
-        color: 'orange',
-        weight: 4,
-        fillOpacity: 0.2,
-        radius: city.population/200000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-    // toLocaleString() will format with a thousands separator
-   });
    
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
